8a24fecbda5150e24ec9bbe1c03d1e19da4ea3b3
HoustonNP <92217081+HoustonNP@users.noreply.github.com>
2022-02-24 14:21:03 +0300

    Add files via upload

---
+++ js/cart.js
@@ -0,0 +1,15 @@
+const cart = () => {
+	const cartBtn = document.querySelector('.button-cart'); //собираем все селекторы с подходящим именем в document и возвращаем первый из них в константу
+	const cart = document.getElementById('modal-cart'); //находим первый эллемент по id в документе и присваиваем в константу
+	const cartClose = cart.querySelector('.modal-close'); // собираем все селекторы в cart
+
+	cartBtn.addEventListener('click', () => {
+		cart.style.display = 'flex'; // свойство подключится инлайново
+	});
+
+	cartClose.addEventListener('click', () => {
+		cart.style.display = ''; // инлайновое свойсво обнулится, но из тега div style не пропадет
+	});
+};
+
+cart();
